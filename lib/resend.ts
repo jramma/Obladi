a0a7c6fc0d113a0bc7ec2b2fc7502f3c821618c5
jramma <jrmasip097@gmail.com>
2025-05-25 16:39:31 +0200

    🗑️ remove resend

--- lib/resend.ts
+++ lib/resend.ts
@@ -1,21 +1,21 @@
  
 
-import { Resend } from "resend";
+// import { Resend } from "resend";
 
-export const resend = new Resend(process.env.RESEND_API_KEY!);
+// export const resend = new Resend(process.env.RESEND_API_KEY!);
 
-export async function sendVerificationEmail(email: string, token: string) {
-  const url = `${process.env.NEXTAUTH_URL}/verify?token=${token}`;
+// export async function sendVerificationEmail(email: string, token: string) {
+//   const url = `${process.env.NEXTAUTH_URL}/verify?token=${token}`;
 
-  await resend.emails.send({
-    from: "noreply@obladi.site",
-    to: email,
-    subject: "Verifica tu correo",
-    html: `
-      <p>Gracias por registrarte.</p>
-      <p>Por favor, verifica tu email haciendo clic en el siguiente enlace:</p>
-      <a href="${url}">Verificar cuenta</a>
-      <p>Este enlace expirará en 30 minutos.</p>
-    `,
-  });
-}
+//   await resend.emails.send({
+//     from: "noreply@obladi.site",
+//     to: email,
+//     subject: "Verifica tu correo",
+//     html: `
+//       <p>Gracias por registrarte.</p>
+//       <p>Por favor, verifica tu email haciendo clic en el siguiente enlace:</p>
+//       <a href="${url}">Verificar cuenta</a>
+//       <p>Este enlace expirará en 30 minutos.</p>
+//     `,
+//   });
+// }
